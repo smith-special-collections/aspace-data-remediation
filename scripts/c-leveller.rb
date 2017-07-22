c44f12bf34e00cf34c78d019b98279de2e28c3e8
Dave Mayo <pobocks@gmail.com>
2017-07-21 21:42:01 -0400

    C02-12 default @levels

---
+++ scripts/c-leveller.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require 'nokogiri'
+
+i = "01" # i is skipped
+c_xpath = 11.times.map do "//c#{i.succ!}" end.join('|')
+
+Dir['eads/*.xml'].each do |fname|
+  xml = Nokogiri.XML(IO.read(fname), nil, 'utf-8')
+
+  write_me = false
+  xml.xpath(c_xpath).each do |c|
+    if !c['level'] || c['level'] == 'unspecified'
+      c['level'] = 'file'
+      write_me = true
+    end
+  end
+
+  if write_me
+    File.open("out/#{File.basename(fname)}", 'w') do |f|
+      f.write xml.to_s
+    end
+  end
+end

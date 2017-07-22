914aed300da8cdf62c0264a20a06a91535c2bade
Dave Mayo <pobocks@gmail.com>
2017-07-21 23:46:25 -0400

    Manually pulled out all the tables!

---
+++ scripts/ref-smoother.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'nokogiri'
+
+Dir['eads/*.xml'].each do |fname|
+  xml = Nokogiri.XML(IO.read(fname), nil, 'utf-8')
+
+  write_me = false
+  xml.xpath('//ref').each do |ref|
+    ref.replace Nokogiri::XML::DocumentFragment.new(xml, ref.content)
+    write_me = true
+  end
+
+  if write_me
+    File.open("out/#{File.basename(fname)}", 'w') do |f|
+      f.write xml.to_s
+    end
+  end
+end
